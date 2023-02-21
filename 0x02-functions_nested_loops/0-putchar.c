766989929f537f741eb9ae1738d61129f282e666
obiokoye <okoyenicholasobi@gmail.com>
2023-02-21 11:41:07 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar and a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('s');
+	_putchar('m');
+	_putchar('h');
+	_putchar('j');
+	_putchar('d');
+	_putchar('a');
+	_putchar('t');
+	_putchar('\n');
+
+	return (0);
+}
