c69c93242395f5c47db08502cdf73a1a7784d5d8
obiokoye <okoyenicholasobi@gmail.com>
2023-03-02 13:50:35 -0800

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,10 +1,9 @@
 #include <stdio.h>                                                                                                                                         int main(void)                                                                                                                        
 {                                                                                                                                     
         int n;                                                                                                                        
-        int a[5];                                                                                                                     
+        int y[5];                                                                                                                     
         int *p;                                                                                                                       
-                                                                                                                                      
-        a[2] = 1024;                                                                                                                  
+        y[2] = 1024;                                                                                                                  
         p = &n;                                                                                                                       
 /*                                                                                                                                    
  * write your line of code here...                                                                                                    
@@ -17,7 +16,7 @@
  */                                                                                                                                   
         *(p + 5) = 98;                                                                                                                
 /* ...so that this prints 98\n */                                                                                                     
-        printf("a[2] = %d\n", a[2]);                                                                                                  
+        printf("y[2] = %d\n", y[2]);                                                                                                  
         return (0);                                                                                                                   
 }
 
