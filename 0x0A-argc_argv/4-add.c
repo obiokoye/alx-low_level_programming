03450ec8dbb9b67a514215216ca1759244387ebb
obiokoye <okoyenicholasobi@gmail.com>
2023-03-10 17:49:31 -0800

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,24 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <ctype.h>
+#include <string.h>
 /**
 * main - entry point
 * @argc: size of argv
 * @argv: array
 * Return: 0
 **/
-
 int main(int argc, char *argv[])
 {
-	int i, mul = 1;
+	int i, j, add = 0;
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
 	for (i = 1; i < argc; i++)
-		mul *= atoi(argv[i]);
-	printf("%d\n", mul);
+	{
+		if (argv[i][0] == '-')
+		{
+			for (j = 1; argv[i][j] != '\0'; j++)
+			{
+				if (isdigit(argv[i][j]) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		}
+		else
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (isdigit(argv[i][j]) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		}
+		add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
 	return (0);
 }
